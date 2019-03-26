8544b950ee77184631542bccbce7a181dcd281ba
Panuwat8296 <48943248+Panuwat8296@users.noreply.github.com>
2019-03-26 15:08:06 +0700

    Add files via upload

---
+++ 6010210562.py
@@ -0,0 +1,6 @@
+Python 2.7.15 (v2.7.15:ca079a3ea3, Apr 30 2018, 16:30:26) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> j = 9
+>>> for i in range(1,10,2):
+	print(" "*j+i*"*")
+	j=j-1
